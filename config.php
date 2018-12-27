4384bab2866a04119906dbfd9ac44d0b08b4a334
hhtmju <study.svfu@gmail.com>
2018-12-28 04:46:32 +0900

    comething new

--- config.php
+++ config.php
@@ -1,14 +1,6 @@
-<<<<<<< HEAD:html/config.php
-<?php
-$server='localhost';
-$user='root';
-$password='';
-$database='meddata';
-=======
 <?php
 $server='localhost';
 $user='user1234';
 $password='user1234';
 $database='meddata';
->>>>>>> 2be376c17647d18ca04e5f72b2af00d9ecc84318:config.php
 ?>
