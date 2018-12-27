fc28b79290a64e17eaf0fc07db04ebe2faafca8a
hhtmju <study.svfu@gmail.com>
2018-12-28 07:29:33 +0900

    comething new

--- add_from.php
+++ add_from.php
@@ -1,11 +1,12 @@
-<meta charset="utf-8">
+
 
 <?php
 	include('config.php');	
 	$link = mysqli_connect($server,$user,$password,$database)
 		or die('Error: Unable to connect: ' . mysqli_connect_error());
-
-
+	
+	mysqli_query($link, "SET NAMES utf8");
+	
 	$name = mysqli_real_escape_string($link, $_POST['name']);
 	$image = mysqli_real_escape_string($link, $_POST['image']);
 	$maker = mysqli_real_escape_string($link, $_POST['maker']);
