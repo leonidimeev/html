dc37e4d5fc487944a46164499fe974e66e6810ff
hhtmju <study.svfu@gmail.com>
2018-12-28 19:54:52 +0900

    comething new

--- add_from.php
+++ add_from.php
@@ -1,14 +1,10 @@
 
 
 <?php
-	error_reporting(-1);
-	header('Content-Type: text/html; charset=utf-8');
-	include('config.php');	
+		include('config.php');	
 	$link = mysqli_connect($server,$user,$password,$database)
 		or die('Error: Unable to connect: ' . mysqli_connect_error());
-	
-	mysql_set_charset('utf8');
-	
+		
 	$name = mysqli_real_escape_string($link, $_POST['name']);
 	$image = mysqli_real_escape_string($link, $_POST['image']);
 	$maker = mysqli_real_escape_string($link, $_POST['maker']);
